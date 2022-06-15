80301c9f2e3a96d173f102ce28598f40a9cdf57f
taewok <taewok5163@naver.com>
2022-06-15 18:47:25 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -15,11 +15,18 @@ const login = () => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(req),
-  }).then((res) => {
-    console.log(res.json());
-  }).then((res)=>{
-    console.log(res)
   })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error("로그인 중 에러 발생");
+    });
 };
 
 btn.addEventListener("click", login);
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -10,7 +10,7 @@ const output = {
 };
 
 const users = {
-  id: ["woorimIT", "나개발", "김팀장"],
+  id: ["taewok516", "나개발", "김팀장"],
   pwd: ["1234", "1234", "123456"],
 };
 
@@ -19,17 +19,17 @@ const process = {
     const id = req.body.id,
       pwd = req.body.pwd;
 
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
+    if (users.id.includes(id)) {//includes()함수는 주어진 요소가 배열에 있는지 여부를 확인합니다 있다면 true 없다면 false
+      const idx = users.id.indexOf(id); //indexOf 함수는, 문자열(string)에서 특정 문자열을 찾고, 검색된 문자열이 '첫번째'로 나타나는 위치 index를 리턴합니다.
       if (users.pwd[idx] === pwd) {
         return res.json({
-          sucess: true,
+          success: true,
         });
       }
     }
 
     return res.json({
-      sucess: false,
+      success: false,
       msg: "로그인에 실패하셨습니다.",
     });
   },
