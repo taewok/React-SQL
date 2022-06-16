3c92db81c0b250abc86af12fdbc34c0d824389e4
taewok <taewok5163@naver.com>
2022-06-16 17:53:50 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -8,14 +8,24 @@ class UserStorage {
   static getUsers(...fields) {
     const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) { //hasOwnProperty() 메소드는 객체가 특정 프로퍼티를 가지고 있는지를  나타내는 boolean 값을 반환한다.
+      if (users.hasOwnProperty(field)) {
+        //hasOwnProperty() 메소드는 객체가 특정 프로퍼티를 가지고 있는지를  나타내는 boolean 값을 반환한다.
         newUsers[field] = users[field];
       }
-      const a = users.id
-      console.log(a)
       return newUsers;
     }, {});
     return newUsers;
+  }
+
+  static getUsersInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const userKeys = Object.keys(users); // =>[id,pwd,name]
+    const userInfo = userKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
   }
 }
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,4 @@
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
   home: (req, res) => {
@@ -11,21 +11,9 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id,
-      pwd = req.body.pwd;
-
-    const users = UserStorage.getUsers("id","pwd");
-    const response = {}
-    if (users.id.includes(id)) {//includes()함수는 주어진 요소가 배열에 있는지 여부를 확인합니다 있다면 true 없다면 false
-      const idx = users.id.indexOf(id); //indexOf 함수는, 문자열(string)에서 특정 문자열을 찾고, 검색된 문자열이 '첫번째'로 나타나는 위치 index를 리턴합니다.
-      if (users.pwd[idx] === pwd) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
-    response.success = false
-    response.msg = "로그인에 실패했습니다."
-    return res.json(response);
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response)
   },
 };
 
